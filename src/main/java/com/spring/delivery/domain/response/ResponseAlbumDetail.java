5fba56defa00982b90445f4df730e4a466bca961
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2024-12-27 08:42:28 +0700

    fix: Tên các trường dữ liệu cho phù hợp với api.

--- src/main/java/com/spring/delivery/domain/response/ResponseAlbumDetail.java
+++ src/main/java/com/spring/delivery/domain/response/ResponseAlbumDetail.java
@@ -1,20 +1,15 @@
 package com.spring.delivery.domain.response;
 
+import com.spring.delivery.domain.ApiPaging;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import java.time.Instant;
-
 @Data
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResponseAlbumCard {
-    long id;
-    String name;
-    String artist;
-    String coverUrl;
-    Instant releaseDate;
+public class ResponseAlbumDetail extends ResponseAlbumCard {
+    ApiPaging<ResponseSongCard> songs;
 }
